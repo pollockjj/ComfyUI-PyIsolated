abb049d95a8eb2e94c5540051ec458590952edc3
John Pollock <pollockjj@gmail.com>
2025-12-05 14:50:25 -0600

    getting working v1 node moved over

--- __init__.py
+++ __init__.py
@@ -1,1 +1,21 @@
-from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS  # noqa
+from __future__ import annotations
+
+from comfy_api.latest import io, ComfyExtension
+
+class PyIsolatedTestNodeV1:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {"required": {}}
+    
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("message",)
+    FUNCTION = "test_v1"
+    CATEGORY = "pyisolated"
+
+    def test(self):
+        return ("PyIsolate is working!",)
+
+
+NODE_CLASS_MAPPINGS = {"PyIsolatedTestNodeV1": PyIsolatedTestNodeV1,}
+
+NODE_DISPLAY_NAME_MAPPINGS = {"PyIsolatedTestNodeV1": "PyIsolatedTestNodeV1",}
